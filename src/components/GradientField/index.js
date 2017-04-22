e07627f9037da69d8ffa2637a5543e5338f2a6eb
Marcus Bernales <mboperator@gmail.com>
2017-04-21 23:14:47 -0700

    colors are lit

--- src/components/GradientField/index.js
+++ src/components/GradientField/index.js
@@ -1,58 +1,53 @@
 import React from 'react';
-import { Observable } from 'rxjs';
+import { Observable, DOM } from 'rxjs';
+
+const s = 50;
 
 class GradientField extends React.Component {
-  componentDidMount() {
-    const colorContext = this.gradientField.getContext('2d');
-
-    this.buildGradient(colorContext);
-    Observable.fromEvent(document, 'mousemove')
-      .map((event) => {
-        debugger;
-        return {
-          colorEventX: event.pageX - colorContext.offset.left,
-          colorEventY: event.pageY - colorContext.offset.top,
-        }
-      })
-      .subscribe((event) => console.log('MOVING', event))
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxX: window.innerWidth,
+      maxY: window.innerHeight,
+    }
   }
 
-  buildGradient = (colorContext) => {
-    const colorGradient = colorContext.createLinearGradient(0, 0, this.gradientField.width, 0);
+  componentDidMount() {
+    const { maxX, maxY } = this.state;
 
-    // Create color gradient
-    colorGradient.addColorStop(0,    "rgb(255,   0,   0)");
-    colorGradient.addColorStop(0.15, "rgb(255,   0, 255)");
-    colorGradient.addColorStop(0.33, "rgb(0,     0, 255)");
-    colorGradient.addColorStop(0.49, "rgb(0,   255, 255)");
-    colorGradient.addColorStop(0.67, "rgb(0,   255,   0)");
-    colorGradient.addColorStop(0.84, "rgb(255, 255,   0)");
-    colorGradient.addColorStop(1,    "rgb(255,   0,   0)");
+    Observable
+    .fromEvent(document, 'mousemove')
+    .subscribe(({ pageX, pageY }) => {
+      const horizontal = (pageX / maxX) * 360;
+      const vertical = ((pageY / maxY) * 10000) / 100;
 
-    // Apply gradient to canvas
-    colorContext.fillStyle = colorGradient;
-    colorContext.fillRect(0, 0, colorContext.canvas.width, colorContext.canvas.height);
+      this.setState({ horizontal, vertical });
+    });
 
-    // // Create semi transparent gradient (white -> trans. -> black)
-    // const bwGradient = colorContext.createLinearGradient(0, 0, 0, this.gradientField.height);
-    // bwGradient.addColorStop(0,   "rgba(255, 255, 255, 1)");
-    // bwGradient.addColorStop(0.5, "rgba(255, 255, 255, 0)");
-    // bwGradient.addColorStop(0.5, "rgba(0,     0,   0, 0)");
-    // bwGradient.addColorStop(1,   "rgba(0,     0,   0, 1)");
-    //
-    // // Apply gradient to canvas
-    // colorContext.fillStyle = bwGradient;
-    // colorContext.fillRect(0, 0, colorContext.canvas.width, colorContext.canvas.height);
+    Observable
+    .fromEvent(window, 'resize')
+    .subscribe(() => {
+      this.setState({
+        maxX: window.innerWidth,
+        maxY: window.innerHeight,
+      });
+    });
+  }
 
+  componentDidUpdate() {
+    const { horizontal, vertical } = this.state;
+
+    const body = document.querySelector('body');
+    body.style.backgroundColor = `hsl(${horizontal}, ${s}%, ${vertical}%)`;
+    console.log(body.style.backgroundColor);
   }
 
   render() {
     const { style } = this.props;
     return (
-      <canvas
-        ref={node => this.gradientField = node}
-        style={style}
-      />
+      <div>
+        Gradient Field
+      </div>
     );
   }
 }
